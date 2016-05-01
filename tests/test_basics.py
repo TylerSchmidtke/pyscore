7a3e53b1c3d7a1a6732532ecf485a72fd2aac308
Tyler Schmidtke <tylerschmidtke@gmail.com>
2016-05-01 14:38:45 -0400

    Making tests work

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,6 +1,7 @@
 import unittest
 from flask import current_app
-from app import create_app, db
+from app import create_app
+from mongoengine import connect
 
 
 class BasicsTestCase(unittest.TestCase):
@@ -8,11 +9,10 @@ class BasicsTestCase(unittest.TestCase):
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
-        db.create_all()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        d = connect('pyscore-test')
+        d.drop_database('pyscore-test')
         self.app_context.pop()
 
     def test_app_exists(self):
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,9 +1,8 @@
 import unittest
-import time
-from datetime import datetime
-from flask import current_app
-from app import create_app, db
-from app.models import User, AnonymousUser, Role, Permission
+from app import create_app
+from app.models import User
+from flask.ext.bcrypt import generate_password_hash
+from mongoengine import connect
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -11,39 +10,18 @@ class UserModelTestCase(unittest.TestCase):
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
-        db.create_all()
-        Role.insert_roles()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        d = connect('pyscore-test')
+        d.drop_database('pyscore-test')
         self.app_context.pop()
 
-    def test_password_setter(self):
-        u = User(password='barbarian')
-        self.assertTrue(u.password_hash is not None)
-
-    def test_no_password_getter(self):
-        u = User(password='barbarian')
-        with self.assertRaises(AttributeError):
-            u.password
-
     def test_password_verification(self):
-        u = User(password='barbarian')
+        u = User(password_hash=generate_password_hash('barbarian'))
         self.assertTrue(u.verify_password('barbarian'))
         self.assertFalse(u.verify_password('thulsa'))
 
     def test_password_salts_are_random(self):
-        u = User(password='barbarian')
-        u2 = User(password='barbarian')
+        u = User(password_hash=generate_password_hash('barbarian'))
+        u2 = User(password_hash=generate_password_hash('barbarian'))
         self.assertTrue(u.password_hash != u2.password_hash)
-
-    def test_roles_and_permissions(self):
-        Role.insert_roles()
-        u = User(email='conan@example.com')
-        self.assertTrue(u.can(Permission.SUBMIT))
-        self.assertFalse(u.can(Permission.CREATE))
-
-    def test_anonymous_user(self):
-        u = AnonymousUser()
-        self.assertFalse(u.can(Permission.SUBMIT))
