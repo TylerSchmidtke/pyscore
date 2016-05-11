40aa8f70717831001fae3e069069ec860e18f176
Tyler Schmidtke <tylerschmidtke@gmail.com>
2016-05-10 23:54:43 -0400

    Remove form iteration to optimize submission chcks

--- app/main/views.py
+++ app/main/views.py
@@ -4,25 +4,34 @@ from fuzzywuzzy import fuzz
 from .forms import ChallengeForm
 from . import main
 from ..models import Challenge, User, Audit
+import collections
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    forms = []
+
+    # Build a OrderedDict of forms for each challenge.
+    # This should work because there should never be duplicate challenge IDs.
+    # Associate challenge details with the form
+    forms = collections.OrderedDict()
+
     for challenge in Challenge.objects(active=True):
         solved = False
-        if current_user.is_authenticated and \
-           challenge.id in current_user.solved_challenges:
+
+        if current_user.is_authenticated and challenge.id in current_user.solved_challenges:
             solved = True
 
-        form = {'f': ChallengeForm(prefix=str(challenge.id)),
-                'challenge_text': challenge.challenge_text,
-                'points': challenge.points,
-                'attachment_path': challenge.attachment_path,
-                'successes': challenge.successes,
-                'failures': challenge.failures,
-                'solved': solved}
-        forms.append(form)
+        forms[str(challenge.id)] = {
+            'f': ChallengeForm(prefix=str(challenge.id)),
+            'challenge_text': challenge.challenge_text,
+            'points': challenge.points,
+            'attachment_path': challenge.attachment_path,
+            'successes': challenge.successes,
+            'failures': challenge.failures,
+            'solved': solved
+        }
+
+    # Check the submission
     if request.method == "POST":
 
         if not current_user.is_authenticated:
@@ -30,60 +39,61 @@ def index():
             return redirect(url_for('main.index'))
 
         # Grab the first key in the POST form data to lookup the challenge and process the submission
-        form_c = Challenge.objects.get(id=str(next(request.form.keys())).split('-')[0])
+        c_id = str(next(request.form.keys()).split('-')[0])
+        form_c = Challenge.objects.get(id=c_id)
+        sub_form = forms[c_id]
 
-        for form in forms:
+        # check the submission data and
+        if sub_form['f'].challenge_submission.data and \
+           sub_form['f'].challenge_submission.data != '' and \
+           sub_form['f'].validate():
+            a = Audit(user=current_user.username,
+                      message_type="user",
+                      ip=request.remote_addr)
+            submission = sub_form['f'].challenge_submission.data.rstrip()
 
-            if form['f'].challenge_submission.data and \
-               form['f'].challenge_submission.data != '' and \
-               form['f'].validate():
-                a = Audit(user=current_user.username,
-                          message_type="user",
-                          ip=request.remote_addr)
-                submission = form['f'].challenge_submission.data.rstrip()
+            if form_c.id not in current_user.solved_challenges:
+                if not form_c.case_sensitive:
+                    submission = submission.lower()
 
-                if form_c.id not in current_user.solved_challenges:
-                    if not form_c.case_sensitive:
-                        submission = submission.lower()
-
-                    if form_c.fuzzy_answer:
-                        ratio = fuzz.token_sort_ratio(form_c.plain_text, form['f'].challenge_submission.data)
-                        if ratio >= 83:
-                            u = User.objects.get(id=current_user.id)
-                            u.update(inc__score=form_c.points, upsert=True)
-                            u.update(push__solved_challenges=form_c.id, upsert=True)
-                            form_c.update(inc__successes=1)
-                            flash("Correct!")
-                            a.message = "Solved " + str(form_c.id) + " (fuzzy) with a ratio of " + str(ratio)
-                            a.save()
-                            return redirect(url_for('main.index'))
-                        else:
-                            form_c.update(inc__failures=1)
-                            flash("Incorrect!")
-                            a.message = "Failed to solve " + str(form_c.id) + " (fuzzy) with a ratio of " + str(ratio)
-                            a.save()
-
-                    elif submission == form_c.plain_text:
+                if form_c.fuzzy_answer:
+                    ratio = fuzz.token_sort_ratio(form_c.plain_text, sub_form['f'].challenge_submission.data)
+                    if ratio >= 83:
                         u = User.objects.get(id=current_user.id)
                         u.update(inc__score=form_c.points, upsert=True)
                         u.update(push__solved_challenges=form_c.id, upsert=True)
                         form_c.update(inc__successes=1)
                         flash("Correct!")
-
-                        a.message = "Solved " + str(form_c.id)
+                        a.message = "Solved " + str(form_c.id) + " (fuzzy) with a ratio of " + str(ratio)
                         a.save()
                         return redirect(url_for('main.index'))
                     else:
                         form_c.update(inc__failures=1)
                         flash("Incorrect!")
-                        a.message = "Failed to solve " + str(form_c.id)
+                        a.message = "Failed to solve " + str(form_c.id) + " (fuzzy) with a ratio of " + str(ratio)
                         a.save()
-                else:
-                    a.message = "Tried to solve " + str(form_c.id) + "again."
-                    a.save()
-                    flash("You've already solved that challenge!")
 
-    return render_template("index.html", challenges=Challenge, forms=forms)
+                elif submission == form_c.plain_text:
+                    u = User.objects.get(id=current_user.id)
+                    u.update(inc__score=form_c.points, upsert=True)
+                    u.update(push__solved_challenges=form_c.id, upsert=True)
+                    form_c.update(inc__successes=1)
+                    flash("Correct!")
+
+                    a.message = "Solved " + str(form_c.id)
+                    a.save()
+                    return redirect(url_for('main.index'))
+                else:
+                    form_c.update(inc__failures=1)
+                    flash("Incorrect!")
+                    a.message = "Failed to solve " + str(form_c.id)
+                    a.save()
+            else:
+                a.message = "Tried to solve " + str(form_c.id) + "again."
+                a.save()
+                flash("You've already solved that challenge!")
+
+    return render_template("index.html", forms=forms)
 
 
 @main.route('/scoreboard')
